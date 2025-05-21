de92db40e9779b3d60f617d461183938ef423b75
Taombawkry <thomas.gondwe@outlook.com>
2025-05-21 12:02:02 +0200

    pet peeves

--- test/PoC.test.ts
+++ test/PoC.test.ts
@@ -1,5 +1,5 @@
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
-import { ethers as hhethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import {IERC20Metadata, UpsideMetaCoin, UpsideProtocol, UpsideStakingStub} from "../types";
 import { expect } from "chai";
 
@@ -17,30 +17,39 @@ describe("C4 PoC Test Suite", function () {
   let sampleLinkToken: UpsideMetaCoin; 
 
   before(async function () {
-    signers = await hhethers.getSigners();
+    signers = await ethers.getSigners();
     owner = signers[2];
     user = signers[0];
     attacker = signers[1];
     user1 = signers[3];
 
-    const upsideStakingStubFactory = await hhethers.getContractFactory("UpsideStakingStub");
-    stakingContract = await upsideStakingStubFactory.connect(owner).deploy(owner.address);
+
+    const StakingStub = await ethers.getContractFactory("UpsideStakingStub");
+    stakingContract = await StakingStub.connect(owner).deploy(owner.address);
     await stakingContract.connect(owner).setFeeDestinationAddress(owner.address);
 
-    const upsideProtocolFactory = await hhethers.getContractFactory("UpsideProtocol");
-    upsideProtocol = await upsideProtocolFactory.connect(owner).deploy(owner.address);
+    const UpsideProtocolFactory = await ethers.getContractFactory("UpsideProtocol");
+    upsideProtocol = await UpsideProtocolFactory.connect(owner).deploy(owner.address);
 
-    // Deploy mock USDC
-    const liquidityTokenFactory = await hhethers.getContractFactory("USDCMock");
-    liquidityToken = await liquidityTokenFactory.connect(owner).deploy();
+    const USDCMock = await ethers.getContractFactory("USDCMock");
+    liquidityToken = await USDCMock.connect(owner).deploy();
 
-    // Setup protocol
     await upsideProtocol.connect(owner).init(await liquidityToken.getAddress());
     await upsideProtocol.connect(owner).setStakingContractAddress(await stakingContract.getAddress());
-    
-    // Deploy a simple Tokenized URL
+
+    // Create the first token
+    await upsideProtocol.connect(owner).tokenize("https://exploit.me", await liquidityToken.getAddress());
+    metaCoin = await ethers.getContractAt(
+      "UpsideMetaCoin",
+      await upsideProtocol.urlToMetaCoinMap("https://exploit.me")
+    );
+
+    // Create the second token for the combined test
     await upsideProtocol.connect(owner).tokenize("https://code4rena.com/", await liquidityToken.getAddress());
-    sampleLinkToken = await hhethers.getContractAt("UpsideMetaCoin", await upsideProtocol.urlToMetaCoinMap("https://code4rena.com/"));
+    sampleLinkToken = await ethers.getContractAt(
+      "UpsideMetaCoin",
+      await upsideProtocol.urlToMetaCoinMap("https://code4rena.com/")
+    );
   });
 
   it("should demonstrate withdrawal cooldown manipulation", async function() {
